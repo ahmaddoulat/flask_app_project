11897643c396c98a5c27b1fba501c999d23d3428
ahmaddoulat <adoulat@uncc.edu>
2020-02-15 20:52:39 -0500

    Update routes.py

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -1,10 +1,117 @@
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required, current_user
 from passlib.hash import sha256_crypt
+import pymongo
+from storygen.storygen import Story
+from sshtunnel import SSHTunnelForwarder
 
 from flask_app import app, db
 from flask_app.models import User, Post
 from flask_app.forms import PostForm
+
+@app.route("/<string:student_id>/<string:structure>", methods=['POST'])
+def student_story_api(student_id, structure):
+    try:
+        citizenship_type = "1" if request.json['citizenship_type'] == "Yes" else "0"
+    except:
+        citizenship_type = '0'
+    try:
+        nation_of_citizenship_desc = "1" if request.json['nation_of_citizenship_desc'] == "Yes" else "0"
+    except:
+        nation_of_citizenship_desc = '0'
+    try:
+        current_age = "1" if request.json['current_age'] == "Yes" else "0"
+    except:
+        current_age = '0'
+    try:
+        primary_ethnicity = "1" if request.json['primary_ethnicity'] == "Yes" else "0"
+    except:
+        primary_ethnicity = '0'
+    try:
+        student_population_desc = "1" if request.json['student_population_desc'] == "Yes" else "0"
+    except:
+        student_population_desc = '0'
+    try:
+        student_population = "1" if request.json['student_population'] == "Yes" else "0"
+    except:
+        student_population = '0'
+    try:
+        admissions_population_desc = "1" if request.json['admissions_population_desc'] == "Yes" else "0"
+    except:
+        admissions_population_desc = '0'
+    try:
+        advisor_count = "1" if request.json['advisor_count'] == "Yes" else "0"
+    except:
+        advisor_count = '0'
+    try:
+        gpa = "1" if request.json['gpa'] == "Yes" else "0"
+    except:
+        gpa = '0'
+    try:
+        credits_attempted = "1" if request.json['credits_attempted'] == "Yes" else "0"
+    except:
+        credits_attempted = '0'
+    try:
+        credits_passed = "1" if request.json['credits_passed'] == "Yes" else "0"
+    except:
+        credits_passed = '0'
+    try:
+        academic_standing_desc = "1" if request.json['academic_standing_desc'] == "Yes" else "0"
+    except:
+        academic_standing_desc = '0'
+
+    features_list = [
+        citizenship_type,
+        nation_of_citizenship_desc,
+        current_age,
+        primary_ethnicity,
+        student_population_desc,
+        student_population,
+        admissions_population_desc,
+        advisor_count,
+        gpa,
+        credits_attempted,
+        credits_passed,
+        academic_standing_desc
+    ]
+
+    selected_features = ''.join(features_list)
+
+    if student_id is None:
+        return {'message': 'There is no student ID', 'data': {}}, 404
+    if selected_features is None:
+        return {'message': 'There is no features selected', 'data': {}}, 404
+    if structure is None:
+        return {'message': 'There is no structure selected', 'data': {}}, 404
+
+    MONGO_HOST = "REMOTE_IP_ADDRESS"
+    MONGO_DB = "eager_la_db"
+    MONGO_USER = "LOGIN"
+    MONGO_PASS = "PASSWORD"
+    print(student_id)
+    print(structure)
+    print('Connecting to pymongo database!!')
+
+    server = SSHTunnelForwarder(
+        "10.18.207.246",
+        ssh_username="adoulat",
+        ssh_password="Reset_my_password1986",
+        remote_bind_address=('127.0.0.1', 27017)
+    )
+
+    server.start()
+
+    client = pymongo.MongoClient('127.0.0.1', server.local_bind_port)  # server.local_bind_port is assigned local port
+    db = client[MONGO_DB]
+    col = db['students_data_cleaned_with_default_stories'].find({'student_id': student_id})
+    student_story = Story(col[0], selected_features)
+
+    if structure == "temporal":
+        return jsonify(student_story.temporal_story), 201
+    elif structure == "default":
+        return jsonify(student_story.default_story), 201
+    elif structure == "outcome":
+        return jsonify(student_story.outcome_story), 201
 
 @app.route("/")
 def index():
