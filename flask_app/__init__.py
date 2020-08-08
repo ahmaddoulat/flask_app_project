2ec85e35c77e494ac6d6ea13cb6820e3f3e56ce3
ahmaddoulat <adoulat@uncc.edu>
2020-08-08 18:44:19 -0400

    Update __init__.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,70 +1,42 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-import pymongo
-from storygen import Story
-from sshtunnel import SSHTunnelForwarder
+from flask import Flask, jsonify, request
+from flask import make_response
+from flask_pymongo import PyMongo
+from flask import jsonify
+import math
+from pprint import pprint
+# from flask_cors import CORS, cross_origin
+
+
+from storygen.storygen import Story
 import json
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/eager_la_db"
 
-app.config['SECRET_KEY'] = '1A37BbcCJh67'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+mongo = PyMongo(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+@app.route("/")
+def index():
+    return "Hello!!!"
 
 @app.route("/<string:student_id>/<string:structure>", methods=['POST'])
 def student_story_api(student_id, structure):
-    try:
-        citizenship_type = "1" if request.json['citizenship_type'] == "Yes" else "0"
-    except:
-        citizenship_type = '0'
-    try:
-        nation_of_citizenship_desc = "1" if request.json['nation_of_citizenship_desc'] == "Yes" else "0"
-    except:
-        nation_of_citizenship_desc = '0'
-    try:
-        current_age = "1" if request.json['current_age'] == "Yes" else "0"
-    except:
-        current_age = '0'
-    try:
-        primary_ethnicity = "1" if request.json['primary_ethnicity'] == "Yes" else "0"
-    except:
-        primary_ethnicity = '0'
-    try:
-        student_population_desc = "1" if request.json['student_population_desc'] == "Yes" else "0"
-    except:
-        student_population_desc = '0'
-    try:
-        student_population = "1" if request.json['student_population'] == "Yes" else "0"
-    except:
-        student_population = '0'
-    try:
-        admissions_population_desc = "1" if request.json['admissions_population_desc'] == "Yes" else "0"
-    except:
-        admissions_population_desc = '0'
-    try:
-        advisor_count = "1" if request.json['advisor_count'] == "Yes" else "0"
-    except:
-        advisor_count = '0'
-    try:
-        gpa = "1" if request.json['gpa'] == "Yes" else "0"
-    except:
-        gpa = '0'
-    try:
-        credits_attempted = "1" if request.json['credits_attempted'] == "Yes" else "0"
-    except:
-        credits_attempted = '0'
-    try:
-        credits_passed = "1" if request.json['credits_passed'] == "Yes" else "0"
-    except:
-        credits_passed = '0'
-    try:
-        academic_standing_desc = "1" if request.json['academic_standing_desc'] == "Yes" else "0"
-    except:
-        academic_standing_desc = '0'
+    features = json.loads(request.data)
+    
+    return "Something wrong"
+
+    citizenship_type = "1" if features['citizenship_type'] == "Yes" else "0"
+    nation_of_citizenship_desc = "1" if features['nation_of_citizenship_desc'] == "Yes" else "0"
+    current_age = "1" if features['current_age'] == "Yes" else "0"
+    primary_ethnicity = "1" if features['primary_ethnicity'] == "Yes" else "0"
+    student_population_desc = "1" if features['student_population_desc'] == "Yes" else "0"
+    student_population = "1" if features['student_population'] == "Yes" else "0"
+    admissions_population_desc = "1" if features['admissions_population_desc'] == "Yes" else "0"
+    advisor_count = "1" if features['advisor_count'] == "Yes" else "0"
+    gpa = "1" if features['gpa'] == "Yes" else "0"
+    credits_attempted = "1" if features['credits_attempted'] == "Yes" else "0"
+    credits_passed = "1" if features['credits_passed'] == "Yes" else "0"
+    academic_standing_desc = "1" if features['academic_standing_desc'] == "Yes" else "0"
 
     features_list = [
         citizenship_type,
@@ -85,37 +57,23 @@ def student_story_api(student_id, structure):
 
     if student_id is None:
         return {'message': 'There is no student ID', 'data': {}}, 404
-    if selected_features is None:
-        return {'message': 'There is no features selected', 'data': {}}, 404
     if structure is None:
         return {'message': 'There is no structure selected', 'data': {}}, 404
 
-#     MONGO_HOST = "REMOTE_IP_ADDRESS"
-#     MONGO_DB = "eager_la_db"
-#     MONGO_USER = "LOGIN"
-#     MONGO_PASS = "PASSWORD"
-#     print(student_id)
-#     print(structure)
-#     print('Connecting to pymongo database!!')
+    student = mongo.db.students_data_cleaned.find({{'student_id': int(student_id)}})
 
-#     server = SSHTunnelForwarder(
-#         "10.18.207.246",
-#         ssh_username="adoulat",
-#         ssh_password="Reset_my_password1986",
-#         remote_bind_address=('127.0.0.1', 27017)
-#     )
+    # try:
+    #     client = MongoClient('localhost', 27017)
+    #     db = client.eager_la_db
+    #     collection = db.students_data_cleaned
+    #     curr_student = collection.find({'student_id': int(student_id)})
+    # except Exception as e:
+    #     return e
+    
+    # return "HI AHMAD!!"
 
-#     server.start()
 
-#     client = pymongo.MongoClient('127.0.0.1', server.local_bind_port)  # server.local_bind_port is assigned local port
-#     db = client[MONGO_DB]
-#     col = db['students_data_cleaned_with_default_stories'].find({'student_id': student_id})
-
-    with open("/home/flask_app_project/students/" + '800580594', 'rb') as f:
-        object_file = json.load(f)
-
-    student_story = Story(object_file, selected_features)
-#     student_story = Story(col[0], selected_features)
+    student_story = Story(student, selected_features)
 
     if structure == "temporal":
         return jsonify(student_story.temporal_story), 201
@@ -123,7 +81,8 @@ def student_story_api(student_id, structure):
         return jsonify(student_story.default_story), 201
     elif structure == "outcome":
         return jsonify(student_story.outcome_story), 201
+    elif structure == "test":
+        return jsonify(selected_features), 201
 
-# from flask_app import routes
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
